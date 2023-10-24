7e364a7821ffe5c65ec2b75954187825110ac1ea
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-10-24 23:33:56 +0200

    feat: scroll for fixed header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,11 +1,38 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './Header.module.scss';
 import ButtonLink from '../ButtonLink';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(window.scrollY > 40);
+  const pathname = usePathname();
+
+  const detectScroll = () => {
+    if (window.scrollY > 40 && !isScrolled) {
+      setIsScrolled(true);
+    } 
+
+    if (window.scrollY <= 40 && isScrolled) {
+      setIsScrolled(false);
+    }
+  }
+
+  useEffect(() => {
+    if (pathname === '/') {
+      window.addEventListener('scroll', detectScroll);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', detectScroll);
+    }
+  })
+
   return (
-    <header className={styles.Header}>
+    <header className={`${styles.Header} fixed z-10 w-full top-0 transition-all ${!isScrolled && pathname === '/' && 'bg-transparent'}`}>
       <div className="container mx-auto px-4 py-5">
         <div className="flex justify-between align-middle items-center">
           <Link href="/">
