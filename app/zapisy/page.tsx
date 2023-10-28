570943d2dfcab4a5ff489974d492f7da97eb308e
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-10-29 00:38:50 +0200

    change copy

--- app/zapisy/page.tsx
+++ app/zapisy/page.tsx
@@ -12,7 +12,7 @@ export default function RegistrationPage() {
     <Container className="max-w-[1090px]">
       <div className="mt-[110px]">
           <SectionTitle title="Zapisy na wydarzenia" subtitle="Zapisy wystartowały" />
-          <p className="text-md text-center max-w-lg mx-auto mb-24">Zapisy będą uruchamiane na 5 dni przed wydarzeniem. Zapraszamy do odwiedzania naszej strony.</p>
+          <p className="text-md text-center max-w-lg mx-auto mb-24">Zapisy będą uruchamiane na 5 dni przed wydarzeniem. Zapraszamy do odwiedzania naszej strony i udziału w wydarzeniach.</p>
           <div className="flex flex-wrap justify-start gap-x-8 gap-y-12">
             {nextEvents.map(event => (
               <RegistrationEventBox {...event} key={event.id} />
