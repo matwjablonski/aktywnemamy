ea0b49e7c35e460e835065b6d5f07dd59511c885
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-10-29 11:06:31 +0100

    add page event title

--- app/wydarzenia/page.tsx
+++ app/wydarzenia/page.tsx
@@ -1,5 +1,6 @@
 import Container from '../components/Container'
 import EventBox from '../components/EventBox';
+import SectionTitle from '../components/SectionTitle';
 import { events } from '../data/events';
 import { getAllSortedEvents, getClosestEvents } from '../utils/events';
 
@@ -9,6 +10,8 @@ const EventsPage = () => {
   return (
     <div className="mt-[110px]">
       <Container>
+        <SectionTitle title="Wydarzenia" subtitle="Co się u nas dzieje?" />
+        <p className="text-md text-center max-w-lg mx-auto mb-16">Poniżej znajdziesz wszystkie wydarzenia, które zrealizowaliśmy i które są jeszcze przed nami.</p>
         {nextEvents.map(event => (
           <EventBox {...event} key={event.id} withArchive showDetails />
         ))}
