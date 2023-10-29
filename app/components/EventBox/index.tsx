93cd87dcfa3c99d36a95e480686c2c3c316c89b5
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-10-29 11:03:56 +0100

    add rwd to events page

--- app/components/EventBox/index.tsx
+++ app/components/EventBox/index.tsx
@@ -25,14 +25,20 @@ const EventBox: FC<EventBox> = ({
 }) => (
   <article className={`
     flex
+    flex-col
+
     content-stretch
 
     mb-6
     px-4
     py-4
     w-8/12
+    md:w-full
+    lg:w-8/12
     min-h-[200px]
     mx-auto
+
+    md:flex-row
 
     ${(withArchive && isEventInPast(eventDate)) ? 'opacity-50 grayscale' : ''}
   `}>
@@ -43,11 +49,12 @@ const EventBox: FC<EventBox> = ({
       overflow-hidden
       rounded-l-lg
 
-      mr-6
+      md:mr-6
       py-4
       pl-8
       pr-4
-      w-[200px]
+      w-full
+      md:w-[200px]
       min-w-[200px]
 
       bg-gradient-to-b
@@ -58,10 +65,10 @@ const EventBox: FC<EventBox> = ({
       text-gray-dark
     ">
       {new Intl.DateTimeFormat('pl-PL', { dateStyle: 'long' }).format(eventDate)}
-      <div className="flex flex-col items-start gap-1">
+      <div className="flex md:flex-col items-start gap-1">
         {canRegister(eventDate) && registrationUrl && (
           <a href={registrationUrl} rel="nofollow noreferrer noopener" target="_blank">
-            <div className="flex gap-2 text-sm items-center text-gray-dark hover:text-secondary transition-all">
+            <div className="flex gap-2 text-sm items-center text-gray-dark hover:text-secondary transition-all whitespace-nowrap">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
               </svg>
@@ -72,7 +79,7 @@ const EventBox: FC<EventBox> = ({
         {
           detailsUrl && (
             <a href={detailsUrl} rel="nofollow noreferrer noopener" target="_blank">
-              <div className="flex gap-2 text-sm items-center text-gray-dark hover:text-secondary transition-all">
+              <div className="flex gap-2 text-sm items-center text-gray-dark hover:text-secondary transition-all whitespace-nowrap">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
@@ -83,8 +90,8 @@ const EventBox: FC<EventBox> = ({
         }
       </div>
     </div>
-    <div className="shadow-2xl flex w-full">
-      <div className="w-[200px] overflow-hidden rounded-l-lg border-y border-l border-main min-w-[200px]">
+    <div className="shadow-2xl flex flex-col md:flex-row w-full">
+      <div className="w-full md:w-[200px] overflow-hidden md:rounded-l-lg min-w-[200px]">
         {
           image && (
             <Image
@@ -97,6 +104,8 @@ const EventBox: FC<EventBox> = ({
                 h-full
                 hover:scale-110
                 transition-all
+
+                min-w-full
               "
             />
           )
@@ -112,22 +121,6 @@ const EventBox: FC<EventBox> = ({
         flex-auto
         rounded-r-lg
         bg-white
-        border-y
-        border-main
-
-        hover:after:-right-[4px]
-
-        after:transition-all
-        after:content
-        after:w-[15px]
-        after:block
-        after:h-full
-        after:absolute
-        after:-right-[8px]
-        after:top-0
-        after:bg-main
-        after:rounded-r-lg
-        after:z-[-1]
       ">
         <div>
           <h3 className="text-2xl mb-1 text-gray-dark">{title}</h3>
@@ -139,14 +132,14 @@ const EventBox: FC<EventBox> = ({
           </div>}
           <p className="text-gray-light text-sm mb-6">{description}</p>
         </div>
-        <div className="flex">
-          <div className="flex mr-6 text-sm items-center text-gray gap-2">
+        <div className="flex flex-col md:flex-row">
+          <div className="flex md:mr-6 text-sm items-center text-gray gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             {new Intl.DateTimeFormat('pl-PL', { timeStyle: 'short' }).format(eventDate)} - {calculateEndHour(eventDate, duration)}
           </div>
-          <div className="flex mr-6 text-sm items-center text-gray gap-2">
+          <div className="flex md:mr-6 text-sm items-center text-gray gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
