685001bf03464ef7cabbb442ebc025201fd0dd58
Mateusz Jabłoński <mail@mateuszjablonski.com>
2023-11-29 20:41:15 +0100

    fix typo

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -9,7 +9,7 @@ export default function ContactPage() {
       <div className="mt-[110px]">
         <SectionTitle title="Kontakt" subtitle="Znajdź nas" />
         <p className="text-md text-center max-w-lg mx-auto mb-4">Cześć, super, że jesteś!</p>
-        <p className="text-md text-center max-w-lg mx-auto mb-24">Nasza inicjatywa cały czas się rozwija dzięki takim osobom jak Ty. Jeśli masz pomysł na kolejne spotkanie, chciałabyś lub chciałbyś zostać prelegentem lub po prostu coś na przekazać - napisz do Nas maila lub odwiedź nasz profil na Instagramie.</p>
+        <p className="text-md text-center max-w-lg mx-auto mb-24">Nasza Inicjatywa cały czas się rozwija dzięki takim osobom jak Ty. Jeśli masz pomysł na kolejne spotkanie, chciałabyś lub chciałbyś zostać prelegentem lub po prostu coś nam przekazać - napisz do nas maila lub odwiedź nasz profil na Instagramie.</p>
         <div className="flex justify-center gap-6 items-center mb-6 md:mb-28">
           <Link href="https://www.instagram.com/aktywne_mamy_w_gminie_dopiewo/" target="_blank" className="flex items-center gap-2">
             <Image src="/brand-instagram.svg" height={50} width={50} alt="" />
