ff91624ce9814a08668aba4b1201167f199ab358
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-04-15 16:52:49 +0200

    nested main nav

--- app/components/MainNav/index.tsx
+++ app/components/MainNav/index.tsx
@@ -24,22 +24,30 @@ const MainNav = () => {
         md:bg-white 
         md:top-auto
       `}>
-        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm">
+        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md md:mb-0 md:text-sm relative group py-2">
           <Link className="whitespace-nowrap" href="/o-inicjatywie">O inicjatywie</Link>
+          <ul className="md:absolute md:hidden md:group-hover:block mt-2 md:mb-0 md:p-2 text-white md:bg-main-dark md:mt-2">
+            <li className="[&:not(:last-child)]:pb-1 hidden md:block">
+              <Link className="whitespace-nowrap" href="/o-inicjatywie">O nas</Link>
+            </li>
+            <li className="[&:not(:last-child)]:pb-1">
+              <Link className="whitespace-nowrap" href="/materialy-dodatkowe">Materiały dodatkowe</Link>
+            </li>
+          </ul>
         </li>
-        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm">
+        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm py-2">
           <Link href="/wydarzenia" className="whitespace-nowrap">Wydarzenia</Link>
         </li>
-        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm">
+        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm py-2">
           <Link href="/news" className="whitespace-nowrap">Aktualności</Link>
         </li>
-        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm">
+        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm py-2">
           <Link href="/opinie" className="whitespace-nowrap">Opinie</Link>
         </li>
-        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm">
+        <li className="uppercase tracking-wider text-white md:text-gray-dark hover:text-gray-light transition-all text-md mb-2 md:mb-0 md:text-sm py-2">
           <Link href="/kontakt" className="whitespace-nowrap">Kontakt</Link>
         </li>
-        <li className="md:hidden mt-8">
+        <li className="md:hidden mt-8  py-2">
           <ButtonLink label="Zapisy" link="/zapisy" className="" />
         </li>
       </ul>
