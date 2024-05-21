2b154ef1bf5c1077b30e1819c712b0ed7c97183d
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-05-21 13:19:29 +0200

    new posters from may 24

--- app/materialy-dodatkowe/page.tsx
+++ app/materialy-dodatkowe/page.tsx
@@ -8,6 +8,34 @@ const PromoMaterialsPage: React.FC = () => (
       <SectionTitle title="Materiały dodatkowe" subtitle="Zobacz nasze plakaty i filmy" />
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-6xl mx-auto mb-12">
         <Image
+          src="/promo/maj2024-so3.webp"
+          alt="Plakat promujący kurs samoobrony dla kobiet z maja 2024 - wersja 3"
+          width={471}
+          height={666}
+          className="max-w-full shadow-lg mx-auto"  
+        />
+        <Image
+          src="/promo/maj2024-so2.webp"
+          alt="Plakat promujący kurs samoobrony dla kobiet z maja 2024 - wersja 2"
+          width={471}
+          height={666}
+          className="max-w-full shadow-lg mx-auto"  
+        />
+        <Image
+          src="/promo/maj2024-so1.webp"
+          alt="Plakat promujący kurs samoobrony dla kobiet z maja 2024 - wersja 1"
+          width={471}
+          height={666}
+          className="max-w-full shadow-lg mx-auto"  
+        />
+        <Image
+          src="/promo/maj2024.webp"
+          alt="Plakat promujący wydarzenia z maja 2024"
+          width={471}
+          height={666}
+          className="max-w-full shadow-lg mx-auto"  
+        />
+        <Image
           src="/promo/kwiecien2024.webp"
           alt="Plakat promujący wydarzenia z kwietnia 2024"
           width={471}
