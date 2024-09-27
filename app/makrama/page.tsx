674327f49170658f00d39772422b7e8a299ff604
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-09-27 22:47:51 +0200

    new makrama link

--- app/makrama/page.tsx
+++ app/makrama/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
 
-  redirect('https://docs.google.com/forms/d/e/1FAIpQLScFF7D-dWb592qet6jfo3LXqMzTlnsFwEoqONniztz3m2Tl-Q/viewform');
+  redirect('https://forms.gle/sdQEJyztYASZWNDEA');
 
   return null;
 }
