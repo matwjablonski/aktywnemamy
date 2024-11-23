016a363df198ecb62732fd0fa13faaffb64c47c4
Mateusz Jabłoński <mail@mateuszjablonski.com>
2024-11-23 22:33:35 +0100

    samoobrona link

--- app/samoobrona/page.tsx
+++ app/samoobrona/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
 
-  redirect('https://forms.gle/GwTsDdcnaGtputwR6');
+  redirect('https://forms.gle/4jzp9VuCYw66mYgH6');
 
   return null;
 }
