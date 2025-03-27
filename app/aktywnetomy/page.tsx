8e84798754dd96586c1cbd248f527a0583a9d3ea
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-03-27 22:05:11 +0100

    new pages

---
+++ app/aktywnetomy/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+
+  redirect('https://docs.google.com/forms/d/e/1FAIpQLSeaK9SZvoySk8geLVZT_exHfEA9dzxn31cu7dBwNC8djuIX_g/viewform');
+
+  return null;
+}
---
+++ app/zajaczek/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+
+  redirect('https://www.canva.com/design/DAGiXkhmffc/bN3VG74cl6MGfYAn7Q3Ggg/edit');
+
+  return null;
+}
