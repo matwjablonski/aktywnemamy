b60e7b1d18ccb90ed373248152088723c2fc4165
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-05-16 23:17:56 +0200

    kwiatki

---
+++ app/kwiatek/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+
+  redirect('https://docs.google.com/forms/d/e/1FAIpQLSeZRRL3_7dhg6seRg8YPUTrBitlZ9CgWOdOsahHgpcfhw9c8w/viewform');
+
+  return null;
+}
